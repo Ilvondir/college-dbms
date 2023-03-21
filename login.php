0b994fea812a21b19ac5da8751159c0426966899
Ilvondir <komsa.m@o2.pl>
2023-03-21 17:33:32 +0100

    table created

--- login.php
+++ login.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/libs/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
